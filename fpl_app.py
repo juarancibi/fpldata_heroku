352ceb2dddbaa4d1f30469198838f8fda70ef88b
juarancibi <67302668+juarancibi@users.noreply.github.com>
2021-04-22 21:24:03 -0400

    Update fpl_app.py

--- fpl_app.py
+++ fpl_app.py
@@ -29,7 +29,7 @@ eventpointsList = list(elements_df.event_points)
 current_gw = events_df.id[events_df['is_current'] == True].tolist()[0]
 
 
-testGraph2 = pd.read_csv('https://raw.githubusercontent.com/juarancibi/fpldata_heroku/main/testGraph.csv').set_index('player name')
+testGraph2 = pd.read_csv('https://raw.githubusercontent.com/juarancibi/fpldata_heroku/main/testGraph.csv?token=AQBPKDFZZNI623L46MN4WFLAQIQ3Y').set_index('player name')
 testGraph2.drop(['id','team','position'], axis=1, inplace=True)
 testGraph2['GW'+' '+str(current_gw)] = list(eventpointsList)
 
